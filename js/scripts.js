13233400db559f2d8269c206471cc03dd7ea7660
Warren Uhrich <hello@warren.codes>
2020-04-09 17:12:54 -0600

    Added example of properties updating.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ import Dog from './Dog.Class.js';
 const myDog = new Dog();
 myDog.bark(); // Calling methods...
 myDog.greeting();
+
+// Updating properties.
+myDog.name  = 'Bear';
+myDog.age   = 13;
+myDog.breed = 'American Eskimo';
+
+myDog.greeting();
