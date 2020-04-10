ccf48c849a2d05e910356fd090c1e8adf1f307a4
Warren Uhrich <hello@warren.codes>
2020-04-09 18:13:40 -0600

    Add Person class; add prototype example.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 import Dog from './Dog.Class.js';
 import { Cat } from './Cat.Class.js';
+import Person from './Person.Class.js';
 
 const myDog = new Dog();
 myDog.bark(); // Calling methods...
@@ -21,3 +22,21 @@ myCat.addToy( 'ball' );
 myCat.addToy( 'scratching post' );
 myCat.addToy( 'mouse' );
 myCat.showToys();
+
+const myPerson = new Person( 'Fred' );
+console.log( myPerson ); // Output object.
+
+// Adding a NEW property after-the-fact using JS' built in "prototype" property.
+// Due to JS "hoisting" all instances of the class become aware of the change...
+// EVEN if they were already instantiated!
+// CAREFUL. Only use this if you CANNOT edit the class of origin!
+// In the case of using an external library, it might make sense to /build on/
+// their existing class... but if YOU built a class and realize you need more,
+// just improve your class directly!
+Person.prototype.age = 0; // New property, "age", added to our "blueprint!" Default is zero.
+
+console.log( myPerson );
+console.log( myPerson.age );
+
+myPerson.age = 37;
+console.log( myPerson );
